60b177464a200a0660a46c091bacd7c3e3649c27
Er-Rajnish-Kumar-Tiwari <tanish281202@gmail.com>
2026-02-28 14:52:41 +0000

    done enw all 28

--- backed/index.js
+++ backed/index.js
@@ -1,39 +1,19 @@
-const processAssesment=require("./Config/engine");
-const express=require("express");
-const app=express();
+const processAssesment = require("./Config/engine");
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 
-const payload={
-    assesment_id:"1234",
-    user_email:"rajnish@gmail.com",
-    idempotency_key:"abcd-1234",
-    questions:[
-        {
-            question_id:"q1",
-            weight:2,
-            answer:[2],
-            correct:[2]
-        },
-        {
-            question_id:"q2",
-            weight:2,
-            answer:[1],
-            correct:[1,3]
-        },
-        {
-            question_id:"q3",
-            weight:2,
-            answer:null,
-            correct:[2]
-        },
-        {
-            question_id:"q4",
-            weight:2,
-            answer:[2,5],
-            correct:[2,4]
-        }
-    ]
+const payload = {
+  assesment_id: 9001,
+  user_email: "devtest@able.com",
+  idempotency_key: "abc-123",
+  questions: [
+    { question_id: 1, weight: 2, correct: [2], answer: [2] },
+    { question_id: 2, weight: 2, correct: [1, 3], answer: [1] },
+    { question_id: 3, weight: 1, correct: [4], answer: null },
+    { question_id: 4, weight: 3, correct: [2, 4], answer: [2, 5] },
+  ],
 };
 
 console.log("first Run:");
